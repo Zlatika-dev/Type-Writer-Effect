1259acbc6a0b3ed8bc47cda0023080abb7fb70a6
Zlata <zlata@ellery.ru>
2020-07-19 21:48:46 +0300

    Type-Writer-Effect

---
+++ main.js
@@ -0,0 +1,16 @@
+const TypeWriter = function(txtElement, words, wait = 3000){
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = '';
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false 
+}
+
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init (){
+  
+}
